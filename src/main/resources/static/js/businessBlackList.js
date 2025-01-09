3020d65cdbd0cad5daae01d02b5f9a71d7a3eccd
104735k <shk773577@gmail.com>
2025-01-09 11:02:45 +0900

    [feat] 기업의 회원차단해제기능 구현 및 오류 수정, 검색기능 추가

---
+++ src/main/resources/static/js/businessBlackList.js
@@ -0,0 +1,86 @@
+$(function () {
+
+    $('.btn-unblock').click(function(event) {
+        event.preventDefault();
+        const checkConfirm = confirm("차단을 해제하시겠습니까?");
+
+        if (checkConfirm) {
+            alert("차단이 해제되었습니다");
+            $(this).closest("form").submit();
+        }
+    });
+
+
+    $("#allBlacklist").click(function () {
+
+        fetch('/Shoots/business/blacklist')
+            .then(response => response.text())
+            .then(data => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(data, 'text/html');
+                const newTable = doc.querySelector('#blacklistContainer table');
+                const blacklistContainer = document.getElementById('blacklistContainer');
+
+                blacklistContainer.innerHTML = '';
+
+                if (newTable && newTable.querySelector('tbody').children.length > 0) {
+                    blacklistContainer.appendChild(newTable);
+                    document.querySelector('caption').textContent = "전체 차단 기록";
+                } else {
+                    blacklistContainer.innerHTML = '<div style="text-align: center; margin: 100px 0 100px 0"><p> 블랙리스트 데이터가 존재하지 않습니다 </p></div>';
+                }
+            })
+            .catch(error => {
+                console.error('Error loading all matches:', error);
+            });
+    });
+
+    $("#blockList").click(function () {
+
+        fetch('/Shoots/business/blacklist?block=true')
+            .then(response => response.text())
+            .then(data => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(data, 'text/html');
+                const newTable = doc.querySelector('#blacklistContainer table');
+                const blacklistContainer = document.getElementById('blacklistContainer');
+
+                blacklistContainer.innerHTML = '';
+
+                if (newTable && newTable.querySelector('tbody').children.length > 0) {
+                    blacklistContainer.appendChild(newTable);
+                    document.querySelector('caption').textContent = "차단 고객";
+                } else {
+                    blacklistContainer.innerHTML = '<div style="text-align: center; margin: 100px 0 100px 0"><p> 현재 차단된 고객이 없습니다 </p></div>';
+                }
+            })
+            .catch(error => {
+                console.error('Error loading all matches:', error);
+            });
+    });
+
+    $("#unblockList").click(function () {
+
+        fetch('/Shoots/business/blacklist?unblock=true')
+            .then(response => response.text())
+            .then(data => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(data, 'text/html');
+                const newTable = doc.querySelector('#blacklistContainer table');
+                const blacklistContainer = document.getElementById('blacklistContainer');
+
+                blacklistContainer.innerHTML = '';
+
+                if (newTable && newTable.querySelector('tbody').children.length > 0) {
+                    blacklistContainer.appendChild(newTable);
+                    document.querySelector('caption').textContent = "차단 해제 고객";
+                } else {
+                    blacklistContainer.innerHTML = '<div style="text-align: center; margin: 100px 0 100px 0"><p> 차단 해제된 고객이 없습니다 </p></div>';
+                }
+            })
+            .catch(error => {
+                console.error('Error loading all matches:', error);
+            });
+    });
+
+});
