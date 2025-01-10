294a943ae613e6a7e528fd2c6ed906a72048ea21
wrqbin <leobin97@gmail.com>
2025-01-10 17:31:13 +0900

    PostComment 관련 몇개 추가하니까 안돌아감 BusinessUserSecruityConfig.java

---
+++ src/main/java/com/Shoots/domain/PostComment.java
@@ -0,0 +1,49 @@
+//package com.Shoots.domain;
+//
+//public class PostComment {
+//    private int num;
+//    private String idx;
+//    private String content;
+//    private String reg_date; // 2020-12-24 16:08:35
+//    private int post_idx;
+//
+//    public int getNum() {
+//        return num;
+//    }
+//
+//    public void setNum(int num) {
+//        this.num = num;
+//    }
+//
+//    public String getIdx() {
+//        return idx;
+//    }
+//
+//    public void setIdx(String idx) {
+//        this.idx = idx;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public String getReg_date() {
+//        return reg_date;
+//    }
+//
+//    public void setReg_date(String reg_date) {
+//        this.reg_date = reg_date;
+//    }
+//
+//    public int getPost_idx() {
+//        return post_idx;
+//    }
+//
+//    public void setPost_idx(int post_idx) {
+//        this.post_idx = post_idx;
+//    }
+//}
--- src/main/resources/static/sql/post_comment.sql
+++ src/main/resources/static/sql/post_comment.sql
@@ -1,17 +1,17 @@
-CREATE TABLE post_comment (
-          comment_idx INT AUTO_INCREMENT PRIMARY KEY, -- 댓글 식별 번호
-          post_idx INT, -- 게시글 아이디
-          comment_ref_id INT, -- 부모 댓글 아이디 (자기참조)
-          writer INT, -- 작성자
-          content TEXT NOT NULL, -- 내용
-          register_date DATETIME DEFAULT CURRENT_TIMESTAMP, -- 등록일
-          FOREIGN KEY (post_idx) REFERENCES post(post_idx) ON DELETE CASCADE, -- 게시글 아이디 외래키
-          FOREIGN KEY (comment_ref_id) REFERENCES post_comment(comment_idx) ON DELETE CASCADE, -- 부모 댓글 아이디 외래키
-          FOREIGN KEY (writer) REFERENCES regular_user(idx) ON DELETE CASCADE -- 작성자 외래키
-);
-
-
-
-
-drop table post_comment;
-
+# CREATE TABLE post_comment (
+#           comment_idx INT AUTO_INCREMENT PRIMARY KEY, -- 댓글 식별 번호
+#           post_idx INT, -- 게시글 아이디
+#           comment_ref_id INT, -- 부모 댓글 아이디 (자기참조)
+#           writer INT, -- 작성자
+#           content TEXT NOT NULL, -- 내용
+#           register_date DATETIME DEFAULT CURRENT_TIMESTAMP, -- 등록일
+#           FOREIGN KEY (post_idx) REFERENCES post(post_idx) ON DELETE CASCADE, -- 게시글 아이디 외래키
+#           FOREIGN KEY (comment_ref_id) REFERENCES post_comment(comment_idx) ON DELETE CASCADE, -- 부모 댓글 아이디 외래키
+#           FOREIGN KEY (writer) REFERENCES regular_user(idx) ON DELETE CASCADE -- 작성자 외래키
+# );
+#
+#
+#
+#
+# drop table post_comment;
+#
