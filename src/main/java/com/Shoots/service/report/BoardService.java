d985dd3d7a23cb3656ef4c45bbe71f87df745809
kdh <kdhmm0325@naver.com>
2025-01-14 17:56:55 +0900

    board test

---
+++ src/main/java/com/Shoots/service/report/BoardService.java
@@ -0,0 +1,7 @@
+package com.Shoots.service.report;
+
+import com.Shoots.domain.Board;
+
+public interface BoardService {
+    void insertBoard(Board board);
+}
---
+++ src/main/java/com/Shoots/service/report/BoardServiceImpl.java
@@ -0,0 +1,18 @@
+//package com.Shoots.service.report;
+//
+//import com.Shoots.domain.Board;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class BoardServiceImpl implements BoardService {
+//
+//    private BoardMapper dao;
+//
+//    public BoardServiceImpl(BoardMapper dao) {
+//        this.dao = dao;
+//    }
+//
+//    void insertBoard(Board board){
+//        dao.insert(board);
+//    }
+//}
