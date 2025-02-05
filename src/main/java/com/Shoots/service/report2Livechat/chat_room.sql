c40c23357bf07970d8cf20ff17b1065091b9bcc7
kdh <kdhmm0325@naver.com>
2025-02-05 17:56:42 +0900

    livechat db update

--- src/main/java/com/Shoots/service/report2Livechat/chat_room.sql
+++ src/main/java/com/Shoots/service/report2Livechat/chat_room.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS chatRoom;
+DROP TABLE IF EXISTS chat_room;
 CREATE TABLE chat_room (
     chatRoom_idx INT AUTO_INCREMENT PRIMARY KEY,
     Title varchar(20) DEFAULT 'new_ChatRoom',
--- src/main/java/com/Shoots/service/report2Livechat/chat_room_log.sql
+++ src/main/java/com/Shoots/service/report2Livechat/chat_room_log.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS chatRoomLog;
-CREATE TABLE chat_room (
+DROP TABLE IF EXISTS chat_room_log;
+CREATE TABLE chat_room_log (
     chatRoom_idx INT AUTO_INCREMENT PRIMARY KEY,
     Title varchar(20) DEFAULT 'new_ChatRoom',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
