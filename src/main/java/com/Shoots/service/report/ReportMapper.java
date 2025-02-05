ae860c36a40429170ca616f297625bf84c6a1ca9
cjookyung <cjookyung@gmail.com>
2025-02-05 15:54:11 +0900

    report 차단

--- src/main/java/com/Shoots/service/report/ReportMapper.java
+++ src/main/java/com/Shoots/service/report/ReportMapper.java
@@ -4,6 +4,7 @@ import com.Shoots.domain.Report;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ReportMapper {
@@ -11,6 +12,6 @@ public interface ReportMapper {
     public List<Report> selectReportedUsers(String reporter);
     public Report selectCheckReportDuplicate(String reporter,String reported,String category);
     public int selectReportedCount(String reported,String category);
-    public List<Report> getReportList(); //report list 가져오기
+    public List<Map<String, Object>> getReportList(Map<String, Object> map); //report list 가져오기
     public int getReportCount(); //총 신고 개수
 }
