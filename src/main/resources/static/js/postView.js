dd5c50cd7d601feacd19716c7514bb3b1dc74d68
kdh <kdhmm0325@naver.com>
2025-02-05 17:10:00 +0900

    report complete

--- src/main/resources/static/js/postView.js
+++ src/main/resources/static/js/postView.js
@@ -76,6 +76,7 @@ function getList(state) {
 		// let reportButton = (Comment.user_id !== $("#loginid").val() && role === 'common') ? `
         let reportButton = (Comment.user_id !== $("#loginid").val()) ? `
             <button class="commentReportButton" data-comment-idx="${Comment.comment_idx}" 
+                    data-comment-content="${Comment.content}"
                     data-writer="${Comment.writer}" data-tidx="${Comment.writer}" 
                     data-toggle="modal" data-target=".c-report-modal" style="color:red; border:none">
                 <img src='../img/report.png' style="width:15px; height:15px">
@@ -145,7 +146,6 @@ function getList(state) {
 
                 // 비밀댓글 스타일 적용
                 let childDisplayContentStyle = isSecretC ? 'color: gray;' : '';
-
                 // 답글의 더보기 버튼 및 수정/삭제 버튼 처리
         let childToolButtons = ($("#loginid").val() === childComment.user_id || $("#loginid").val() == 'admin') ? ` 
             <div class='comment-tool'>
@@ -165,6 +165,7 @@ function getList(state) {
         //reportButton 답글쪽
                 reportButton = (childComment.user_id !== $("#loginid").val()) ? `
             <button class="commentReportButton" data-comment-idx="${childComment.comment_idx}" 
+                    data-comment-content="${childComment.content}"
                     data-writer="${childComment.writer}" data-tidx="${childComment.writer}" 
                     data-toggle="modal" data-target=".c-report-modal" style="color:red; border:none">
                 <img src='../img/report.png' style="width:15px; height:15px">
@@ -225,7 +226,7 @@ $(document).on('click', '#titleReport', function() {
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="modalTitle">${reported}님의 댓글 신고</h5>
+                                                <h5 class="modal-title" id="modalTitle">${reported} 신고</h5>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
@@ -245,7 +246,7 @@ $(document).on('click', '#titleReport', function() {
                                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
                                                 
                                                 <button type="button" class="btn btn-primary" 
-                                                onclick="ReportSubmitButton({'category': 'POST'})">
+                                                onclick="ReportSubmitButton({'category': 'POST', 'reportedUser' : '${reported}'})">
                                                     신고하기
                                                 </button>                                            
                                             </div>
@@ -267,7 +268,7 @@ $(document).on('click', '#titleReport', function() {
 $(document).on('click', '.commentReportButton', function() {
     const commentNickname = $(this).closest('.comment-box').find('.comment-nickname');
     const dataCommentIdx= $(this).data('comment-idx');
-    alert(dataCommentIdx);
+    const dataCommentContent = $(this).data('comment-content');
     const modalHtml = `
                                 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="modalTitle">
                                     <div class="modal-dialog">
@@ -293,7 +294,7 @@ $(document).on('click', '.commentReportButton', function() {
                                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
                                                 
                                                 <button type="button" class="btn btn-primary" 
-                                                onclick="ReportSubmitButton({'category': 'COMMENT', 'commentIdx': ${dataCommentIdx}})">
+                                                onclick="ReportSubmitButton({'category': 'COMMENT', 'commentIdx': ${dataCommentIdx}, 'reportedUser' : '${dataCommentContent}'})">
                                                     신고하기
                                                 </button>                                            
                                             </div>
@@ -325,12 +326,13 @@ $(document).on('click', '.commentReportButton', function() {
 function ReportSubmitButton(paramData){
     const category = paramData?.category ?? '';
     const selectedOption = $('select[name="title"]').val();
+
     if(selectedOption == null){
         alert('신고 사유를 선택해주세요.');
         return false;
     }
     var reqData = {
-        reportedUser : String($('#modalReported').val()),
+        reportedUser : paramData?.reportedUser,
         category : category,
         content : selectedOption,
         detail : $('#modalEtcContent').val(),
