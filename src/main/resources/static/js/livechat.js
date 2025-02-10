51ac13aa130e17a8d205bc0f2ad891333b6b5c66
kdh <kdhmm0325@naver.com>
2025-02-10 16:50:04 +0900

    livechat SockeJS URL update3

--- src/main/resources/static/js/livechat.js
+++ src/main/resources/static/js/livechat.js
@@ -30,7 +30,7 @@ function connect(event) {
     if(username) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
-        var socket = new SockJS('https://3.36.152.102:1000/Shoots/livechat/livechat/ws'); // 포트에 맞게 수정
+        var socket = new SockJS('https://goshoots.site/Shoots/livechat/livechat/ws'); // 포트에 맞게 수정
         // var socket = new SockJS('http://localhost:1000/Shoots/livechat/livechat/ws'); // 포트에 맞게 수정
         stompClient = Stomp.over(socket);
         stompClient.connect({}, onConnected, onError);//1헤더 2성공 3실패
