44a9c932696899ba657199c30ba4a8eb6aa95941
youngsoo0206 <ejqlxmrghk@gmail.com>
2025-02-12 17:18:33 +0900

    경로수정 2

--- src/main/java/com/Shoots/WebConfig.java
+++ src/main/java/com/Shoots/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // /upload/** 요청을 C 드라이브의 c:/upload/ 디렉토리로 매핑
         registry.addResourceHandler("/upload/**")
-                .addResourceLocations("file:///c:/upload/");  // 절대 경로
+                .addResourceLocations("file:///upload/");  // 절대 경로
     }
 
 //    @Override
